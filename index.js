d021d0d4abd2f4275d1fedd4d8437f3144c4ccdb
Henok Kirubel <henokhm2@gmail.com>
2024-04-17 21:38:16 +0300

    Add axios

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const axios = require('axios');
 require('dotenv').config();
 
 const { logger } = require('./src/config/logger');
@@ -7,7 +8,24 @@ const PORT = process.env.PORT || 4000;
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello World !'));
+
+app.get('/', async (req, res) => {
+  try {
+    const response = await axios.get(
+      'https://jsonplaceholder.typicode.com/todos',
+    );
+
+    const todos = response.data
+      .slice(0, 10)
+      .map((todo) => `<li>${todo.title}</li>`)
+      .join('');
+
+    return res.send(`Hello there! Your to-do list:<br/><br/>${todos}`);
+  } catch (err) {
+    logger.error(`Error fetching data from jsonplaceholder: Error: ${err}`);
+    return res.status(500).send('Internal server error');
+  }
+});
 
 app.listen(PORT, () => {
   logger.info(`Example app listening on port ${PORT}`);
