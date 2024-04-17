b2d4f12239d4a8594dc357880e4663e663371db3
Henok Kirubel <henokhm2@gmail.com>
2024-04-17 21:28:12 +0300

    Add basic express server

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+require('dotenv').config();
+
+const PORT = process.env.PORT || 4000;
+
+const app = express();
+
+app.get('/', (req, res) => res.send('Hello World !'));
+
+app.listen(PORT, () => console.log(`App listening to port ${PORT}`));
